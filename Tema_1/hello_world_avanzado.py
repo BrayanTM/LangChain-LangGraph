0e5ea0f5c294662241e1fde6f3780a74e1752cee
Brayan Tebelan <brayantebelan@gmail.com>
2026-04-20 22:15:23 -0600

    Tu primera cadena (Chain) con LangChain

--- Tema_1/hello_world_avanzado.py
+++ Tema_1/hello_world_avanzado.py
@@ -1,16 +1,22 @@
 import dotenv
-import logging
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 
 dotenv.load_dotenv()
-logging.basicConfig(level=logging.INFO)
 
 
 llm = ChatOpenAI(model="gpt-5.4-mini", temperature=0.7)
+
 
 plantilla = PromptTemplate(
     input_variables=["nombre"],
     template="Saluda al usuario con su nombre.\nNombre del usuario: {nombre}\nAsistente: ",
 )
+
+chain = plantilla | llm | StrOutputParser()
+
+
+resultado = chain.invoke("Brayan")
+print("Respuesta del modelo: ", resultado)
