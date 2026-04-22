f626f68449a90108ca86c3ae1c39925bca415c10
Brayan Tebelan <brayantebelan@gmail.com>
2026-04-21 22:21:59 -0600

    Proyecto: Configuración inicial del proyecto

--- Tema_1/streamlit_chatbot.py
+++ Tema_1/streamlit_chatbot.py
@@ -1,3 +1,16 @@
+import dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 import streamlit as st
+
+dotenv.load_dotenv()
+
+# Configurar la página de la aplicación
+st.set_page_config(page_title="Chatbot Basico", page_icon="🤖")
+st.title("🤖 Chatbot basico con LangChain y Streamlit")
+st.markdown(
+    "### Este es un ***chatbot de ejemplo*** construido con **LangChain + Streamlit**. ¡Escribe tu mensaje abajo para comenzar!"
+)
+
+
+chat_model = ChatOpenAI(model="gpt-5.4-mini", temperature=0.5)
