dbee6f804ec2e55c0ee6290a9052a817f2a9ffc6
Brayan Tebelan <brayantebelan@gmail.com>
2026-04-21 22:35:51 -0600

    Proyecto: Tipos de mensajes en LangChain: AI, Human, System Message

--- Tema_1/streamlit_chatbot.py
+++ Tema_1/streamlit_chatbot.py
@@ -19,3 +19,15 @@ chat_model = ChatOpenAI(model="gpt-5.4-mini", temperature=0.5)
 # Inicializar el historial de mensajes
 if "mensajes" not in st.session_state:
     st.session_state.mensajes = []
+
+# Mostrar mensajes previos en la interfaz
+
+for msg in st.session_state.mensajes:
+    if isinstance(msg, SystemMessage):
+        # No muestro el mensaje en pantalla
+        continue
+
+    role = "assistant" if isinstance(msg, AIMessage) else "user"
+
+    with st.chat_message(role):
+        st.markdown(msg.content)
