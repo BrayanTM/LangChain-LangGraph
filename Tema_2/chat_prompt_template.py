545aea1956cb0f7e3f5bd558b9b450b1f9397d1b
Brayan Tebelan <brayantebelan@gmail.com>
2026-08-12 20:32:15 -0600

    ChatPromptTemplate: Plantillas de prompts para chats

---
+++ Tema_2/chat_prompt_template.py
@@ -0,0 +1,15 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+chat_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Eres un traductor del español al inglés muy preciso."),
+        ("human", "{texto}"),
+    ]
+)
+
+
+messages = chat_prompt.format_messages(texto="Hola mundo, ¿cómo estás?")
+
+
+for m in messages:
+    print(f"{type(m)}: {m.content}")
