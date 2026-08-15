629626cc66777c735c93cdffc8f4dc6cbe472fd0
Brayan Tebelan <brayantebelan@gmail.com>
2026-08-14 18:43:52 -0600

    Pydantic: Validar y procesar datos con Python y LangChain

---
+++ Tema_2/output_parsers.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class Usuario(BaseModel):
+    id: int
+    nombre: str
+    activo: bool = True
+
+
+data = {"id": "123", "nombre": "ana"}
+
+
+usuario = Usuario(**data)
+
+
+print(usuario.model_dump_json())
