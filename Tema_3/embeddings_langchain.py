bfcb1670eefa8837e340a253ccd5f41c78394cee
Brayan Tebelan <brayantebelan@gmail.com>
2026-09-07 18:22:41 -0600

    Embeddings con LangChain

---
+++ Tema_3/embeddings_langchain.py
@@ -0,0 +1,20 @@
+from langchain_openai import OpenAIEmbeddings
+import numpy as np
+
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+
+texto1 = "La capital de Francia es Paris."
+texto2 = "Paris es la ciudad capital de Francia."
+
+
+vec1 = embeddings.embed_query(texto1)
+vec2 = embeddings.embed_query(texto2)
+
+
+print(f"Dimension de los vectores: {len(vec1)}")
+cos_sim = np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+
+
+print(f"Similitud coseno entre vec1 y vec2: {cos_sim:.3f}")
